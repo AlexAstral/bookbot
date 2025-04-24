a0485527bef80f9b614d3b6450b77828d925e8e1
AlexAstral <alexanderrosslawson@gmail.com>
2025-04-24 21:45:51 +0100

    Fully completed the bookbot as my first project!

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 def main():
-    filepath = "books/frankenstein.txt"
+    import sys
+    filepath = ""
+    if len(sys.argv) == 2:
+        filepath = sys.argv[1]
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    from stats import get_book_text
     from stats import get_num_words
     from stats import count_characters
+    from stats import sort_on
+    from stats import sort_report
+
+    sorted = sort_report(filepath)
     word_count = get_num_words(filepath)
     letter_count = count_characters(filepath)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book fount at {filepath}...")
+    print("----------- Word Count ----------")
     print(word_count)
-    print(letter_count)
+    print("--------- Character Count -------")
+    for i in range(0, len(sorted)):
+        dict_list = sorted[i]
+        letter = dict_list["char"]
+        number = dict_list["num"]
+        print(f"{letter}: {number}")
+        
+    print("============== END ==============")
 main()
--- stats.py
+++ stats.py
@@ -1,4 +1,4 @@
-filepath = "books/frankenstein.txt"
+
 
 #define a function to take a txt file and return as a string
 def get_book_text(filepath):
@@ -7,9 +7,6 @@ def get_book_text(filepath):
         file_contents = f.read()
     return file_contents
 
-book = get_book_text(filepath)
-lower_book = book.lower()
-
 #define function as a .split function and counts number of indices
 def get_num_words(filepath):
     book = get_book_text(filepath)
@@ -17,7 +14,7 @@ def get_num_words(filepath):
     word_list = book.split()
     for i in range(0, len(word_list)):
         num_words += 1
-    return f"{num_words} words found in the document"
+    return f"Found {num_words} total words"
 
 #take portion of previous function to split text and then loop to a dict for counting
 def count_characters(filepath):
@@ -30,5 +27,26 @@ def count_characters(filepath):
                  count[char] = 1
             else:
                  count[char] += 1
-    return count             
+    return count   
+
+def sort_on(dict):
+    return dict["num"]
+
+def sort_report(filepath):
+    count = count_characters(filepath)
+    new_count = []
      
+    for char in count:
+        num = count[char]
+        new_dict = {}
+        symbols = []
+        if char.isalpha():
+            new_dict["char"] = char
+            new_dict["num"] = num
+            new_count.append(new_dict)
+        else:
+            symbols.append(char)
+    new_count.sort(reverse=True, key=sort_on)
+    return new_count
+
+    
